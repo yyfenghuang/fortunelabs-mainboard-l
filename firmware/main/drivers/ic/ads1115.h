5d9d44ed105ec0a19ecc51e4bc907b43ce7b0855
yyfenghuang <lukitrisetiawan2505@gmail.com>
2026-06-20 21:39:06 +0700

    feat: adding header files for ads1115 driver

--- firmware/main/drivers/ic/ads1115.h
+++ firmware/main/drivers/ic/ads1115.h
@@ -16,7 +16,7 @@ extern "C"{
     // Register Map
     #define ADS1115_REG_CONVERSION 0x00
     #define ADS1115_REG_CONFIG 0x01
-    #define ADS1115_REG_LO_TRESHOLD 0x02
+    #define ADS1115_REG_LO_THRESHOLD 0x02
     #define ADS1115_REG_HI_THRESHOLD 0x03
 
     //Register Reset Value
@@ -53,6 +53,18 @@ extern "C"{
         ADS1115_PGA_0_256V = 5, //0b101 -> ±0.256V FSR
     } ads1115_pga_t;
 
+    //Output Data rate
+    typedef enum{
+        ADS1115_ADDR_8SPS = 0, //0b000 -> 8SPS, ~125ms conversion
+        ADS1115_ADDR_16SPS = 1, //0b001 -> 8SPS, ~62.5ms conversion
+        ADS1115_ADDR_32SPS = 2, //0b010 -> 8SPS, ~31.25ms conversion
+        ADS1115_ADDR_64SPS = 3, //0b011 -> 8SPS, ~15.6ms conversion
+        ADS1115_ADDR_128SPS = 4, //0b100 -> 8SPS, ~7.8ms conversion
+        ADS1115_ADDR_250SPS = 5, //0b101 -> 8SPS, ~4.0ms conversion
+        ADS1115_ADDR_475SPS = 6, //0b110 -> 8SPS, ~2.1ms conversion
+        ADS1115_ADDR_860SPS = 7, //0b111 -> 8SPS, ~1.2ms conversion
+    } ads1115_data_rate_t;
+
     //Per-channel config
     //This decision i make for mainboard flexibiliity, instead using global config
     typedef struct 
@@ -60,14 +72,14 @@ extern "C"{
         ads1115_channel_t channel;
         ads1115_pga_t pga;
         ads1115_rate_t data_rate;
-    } ads1115_channel_cfg_t;
+    } ads1115_channel_config_t;
 
     //Driver instance
     typedef struct 
     {
         i2c_bus_t *bus;
         ads1115_addr_t addr;
-        ads1115_channel_cfg_t channel_cfg[4];
+        ads1115_channel_config_t channel_cfg[4];
         bool initialized;
     } ads1115_dev_t;
 
@@ -76,7 +88,7 @@ extern "C"{
     {
         i2c_bus_t *bus;
         ads1115_addr_t addr;
-        ads1115_channel_cfg_t channel_cfg[4];
+        ads1115_channel_config_t channel_cfg[4];
     } ads1115_config_t;
 
     //Driver vTable
@@ -90,9 +102,23 @@ extern "C"{
         /**
          * Trigger single shot
          */
-        esp_err_t (*read)(ads1115_dev_t *dev, ads1115_channel_cfg_t)
-    };
+        esp_err_t (*read)(ads1115_dev_t *dev, ads1115_channel_config_t);
+        /**
+         * Write factory default value to config register
+         */
+        esp_err_t(*reset)(ads1115_dev_t *dev);
+        /**
+         * De-init all resource held by driver instance
+         */
+        void(*deinit)(ads1115_dev_t *dev);
+    } ads1115_driver_t;
     
-    
-    
+    //Concrete Driver Access
+    /**
+     * Return pointer into singleton concrete driver vTable
+     */
+    const ads1115_driver_t *ads1115_get_driver(void);
+
+    #ifdef __cplusplus
 }
+#endif
